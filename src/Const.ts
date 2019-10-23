c9e7b3984938fc8bf1ef4b0c2162b3c9d69295ca
Seth Lewis <lewiscatenary@gmail.com>
2019-10-23 16:03:48 -0500

    production bugfix

--- src/Const.ts
+++ src/Const.ts
@@ -1,4 +1,4 @@
 export const APIENDPOINT =
-  " ec2-18-188-221-109.us-east-2.compute.amazonaws.com";
+  "http://ec2-18-188-221-109.us-east-2.compute.amazonaws.com/api";
 
 // export const APIENDPOINT = "http://localhost:3001/api";
